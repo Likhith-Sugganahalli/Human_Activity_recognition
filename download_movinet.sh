5d3d8e1a47c8afa9ea7c8cf5308957deef200ca1
likhith sugganahalli <likhith.sugganahalli@gmail.com>
2022-04-11 00:08:59 +0530

    ls-adding scripts to download movinet and slowfast models for local inference

---
+++ download_movinet.sh
@@ -0,0 +1,5 @@
+wget https://storage.googleapis.com/tfhub-modules/tensorflow/movinet/a2/stream/kinetics-600/classification/3.tar.gz
+#mv ./3.tar.gz ./movinet_a2_stream_kinetics-600_classification_3.tar.gz
+mkdir ./movinet_a2_stream_kinetics-600_classification_3
+tar -xf 3.tar.gz -C ./movinet_a2_stream_kinetics-600_classification_3
+rm 3.tar.gz
---
+++ download_slowfast.sh
@@ -0,0 +1,1 @@
+wget https://dl.fbaipublicfiles.com/pytorchvideo/model_zoo/kinetics/SLOWFAST_8x8_R50.pyth
